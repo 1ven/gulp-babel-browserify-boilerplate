f2af9071e403b0ef84959e4192da42591bc0c4dc
Aiven Reach <aiven.reach@gmail.com>
2015-10-28 22:19:52 +0300

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	del = require('del'),
-	babel = require('gulp-babel')
+	uglify = require('gulp-uglify'),
+	source = require('vinyl-source-stream'),
+	buffer = require('vinyl-buffer'),
+	browserify = require('browserify'),
+	babelify = require('babelify'),
+	watchify = require('watchify'),
 	rename = require('gulp-rename');
 
 gulp.task('clean', function(cb) {
@@ -16,16 +21,6 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('babel', function() {
-	gulp.src('source/index.js')
-		.pipe(babel())
-		.pipe(rename({
-			basename: 'script'
-		}))
-		.pipe(gulp.dest('build/js'))
-		.pipe(connect.reload());
-});
-
 gulp.task('copy', function() {
 	gulp.src('source/html/*.html')
 		.pipe(gulp.dest('build/'))
@@ -37,6 +32,41 @@ gulp.task('watch', function() {
 	gulp.watch('source/html/*.html', ['copy']);
 });
 
+gulp.task('browserify', function() {
+	var bundler = browserify({
+			entries: ['source/index.js'],
+			transform: [
+				babelify.configure({
+					plugins: ["jsx-control-statements/babel"]
+				})
+			],
+			debug: true,
+			cache: {},
+			packageCache: {},
+			fullPaths: true
+		})
+
+	var watcher = watchify(bundler);
+
+	return watcher
+		.on('update', function() {
+			var updateStart = Date.now();
+			console.log('Updating!');
+			watcher
+				.bundle()
+				.pipe(source('client.min.js'))
+				.pipe(buffer())
+				// .pipe(uglify())
+				.pipe(gulp.dest('build'));
+			console.log('Updated!', (Date.now() - updateStart) + 'ms');
+		})
+		.bundle()
+		.pipe(source('client.min.js'))
+		.pipe(buffer())
+		// .pipe(uglify())
+		.pipe(gulp.dest('build'));
+});
+
 gulp.task('default', ['clean'], function() {
-	gulp.start('connect', 'copy', 'babel', 'watch');
+	gulp.start('connect', 'copy', 'browserify', 'watch');
 });
